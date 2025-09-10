50bc099ab8681a56fe3d818fb1b19f2cfad18b8e
Anssoumane Sissokho <152038588+anssoumane14@users.noreply.github.com>
2025-09-10 11:03:50 +0000

    update champion

--- test.py
+++
@@ -1,9 +0,0 @@
-
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-df = pd.read_excel("data/df_team_playoff_stats_pg.xlsx")
-st.set_page_config(layout="wide")
-
-st.dataframe(df)
