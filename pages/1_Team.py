000b2b57697e992d23c10b623f758f0e4b9b2106
Anssoumane Sissokho <152038588+anssoumane14@users.noreply.github.com>
2025-09-12 15:15:04 +0000

    resolved page

--- pages/1_Team.py
+++ pages/1_Team.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from nav import navbar  # si tu veux l'utiliser plus tard
 
 # -------------------------------
 # Configuration de la Page
